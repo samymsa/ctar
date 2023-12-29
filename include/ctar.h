e0e8084bcf234e4c10c8acf5b5a733d961b0b735
ybokhari <younes.bokhari@outlook.com>
2023-12-29 12:00:13 +0100

    add compress functions to ctar.c

--- include/ctar.h
+++ include/ctar.h
@@ -21,8 +21,36 @@ int ctar_list(ctar_args *args);
 int ctar_list_entry(ctar_header *header, bool verbose);
 
 /**
- * @brief Convert an octal string to a decimal integer.
+ * Compress archive using zlib compression algorithm.
+ *
+ * @param input A pointer to the archive to be compressed.
+ * @param output A pointer to the buffer where the compressed archive will be stored.
+ * @param output_size The size of the output buffer.
+ * @return A z_stream struct containing information about the compression process.
+ */
+z_stream compress_archive(char *input, char *output, size_t output_size);
+
+/**
+ * Decompress archive using zlib decompression algorithm.
+ *
+ * @param input A pointer to the compressed archive.
+ * @param output A pointer to the buffer where the decompressed archive will be stored.
+ * @param output_size The size of the output buffer.
+ * @param defstream A z_stream struct containing the state of the previous compression.
+ * @return A z_stream struct containing information about the decompression process.
+ */
+z_stream decompress_archive(char *input, char *output, size_t output_size, z_stream defstream);
+
+/**
+ * Test compression and decompression using zlib.
  * 
+ * @return 0 if successful, 1 otherwise.
+ */
+int test_compression();
+
+/**
+ * @brief Convert an octal string to a decimal integer.
+ *
  * @param oct The octal string.
  * @param size The size of the string,
  * @return int The decimal integer.
